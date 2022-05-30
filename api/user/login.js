d19fab6c930bd81df911dc7ca6a9bbd98231ef4e
Nguyen Thanh Trong <tronglagi111222@gmail.com>
2022-05-30 16:46:43 +0700

    create new file for api

--- api/user/login.js
+++ api/user/login.js
@@ -11,6 +11,35 @@ const SendInfo = JSON.stringify({
 });
 
 
+// function render(items) {
+//   let htmlDiv = document.getElementById("list-test");
+//   let content = items.map(function (item) {
+//     return `
+//     <h2 class="header-prd"> ${item.id} </h2>
+//     <div class="product">
+//                 <div class="info">
+//                   <p class="name"><a href="#">${item.email}</a></p>
+//                   <p class="vote">
+//                     <span><i class="fas fa-star"></i></span>
+//                     <span><i class="fas fa-star"></i></span>
+//                     <span><i class="fas fa-star"></i></span>
+//                     <span><i class="fas fa-star"></i></span>
+//                     <span><i class="fas fa-star"></i></span>
+//                   </p>
+//                   <p class="desc"> (Size lớn, trắng sữa)</p>
+    
+//                   <p class="price">
+//                     <span>
+//                       ${item.roles[0].name}
+//                     </span> VNĐ
+//                   </p>
+//                 </div>
+//               </div>
+//     `;
+//   });
+//   htmlDiv.innerHTML = content.join("");
+// }
+
 
 // var xhr = new XMLHttpRequest();
 // xhr.open("POST", "http://localhost:8080/api/v1/user/save", false);
